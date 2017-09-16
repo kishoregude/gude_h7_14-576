b9764132ed7c22fabde14e0594ee2b07f4a6d28a
kishoregude <32009568+kishoregude@users.noreply.github.com>
2017-09-16 14:45:54 +0530

    java coded files

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+Student st = new Student(01,"Harikrihna",new Date(1996,01,06),63.568);
+		Student st2 = new Student(02,"KISHORE",new Date(1996,01,06),63.568);
+		Student st3 = new Student(03,"KISHORE",new Date(1996,01,06),63.568);
+		Student st4 = new Student(04,"KISHORE",new Date(1996,01,06),63.568);
+		
+	}
+
+}
